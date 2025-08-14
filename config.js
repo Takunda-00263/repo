74f4c836b4a6bc15c7d71cae36d78969260e307f
Keith <keithtec57@gmail.com>
2025-08-14 17:04:30 +0200

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",   // reply on mentioned message 
 
-MENU_IMAGE_URL: getConfig("MENU_IMAGE_URL") || "https://files.catbox.moe/68qmj5.jpg",  
+MENU_IMAGE_URL: getConfig("MENU_IMAGE_URL") || "https://ibb.co/4wQD4n05",  
 // Bot's "alive" menu mention image
 
    ANTI_DELETE: process.env.ANTI_DELETE || "false",
